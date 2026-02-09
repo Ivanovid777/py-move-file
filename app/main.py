c9aeb3293f228989e6635d33625636ef6f60085a
Ivan <ivanqqq@ukr.net>
2026-02-09 22:00:07 +0100

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,28 +1,20 @@
 from os import makedirs, remove, path
 
-# write your code here
 def move_file(command: str) -> None:
     parsed_cmd = command.split()
-    if len(parsed_cmd) == 3:
-        cmd = parsed_cmd[0]
+    if len(parsed_cmd) == 3 and parsed_cmd[0] == "mv":
         origin_file = parsed_cmd[1]
-        dest = path.dirname(parsed_cmd[2])
-        new_file = path.basename(parsed_cmd[2])
-        if cmd == "mv":
-            if not dest:
-                with (
-                    open(origin_file, "r") as origin,
-                    open(new_file, "w") as dest_file
-                ):
-                    data = origin.read()
-                    dest_file.write(data)
-            else:
-                if not path.exists(dest):
-                    makedirs(dest)
-                with (
-                    open(origin_file, "r") as origin,
-                    open(path.join(dest, new_file), "w") as dest_file
-                ):
-                    data = origin.read()
-                    dest_file.write(data)
-            remove(origin_file)
+        dest_path = parsed_cmd[2]
+        dest_dir = path.dirname(dest_path)
+        new_file = path.basename(dest_path)
+        if dest_path.endswith("/") or not new_file:
+            new_file = path.basename(origin_file)
+        if not dest_dir:
+            dest_dir = "."
+        if not path.exists(dest_dir):
+            makedirs(dest_dir)
+        with open(origin_file, "r") as origin, open(path.join(dest_dir, new_file), "w") as dest_file:
+            data = origin.read()
+            dest_file.write(data)
+
+        remove(origin_file)
